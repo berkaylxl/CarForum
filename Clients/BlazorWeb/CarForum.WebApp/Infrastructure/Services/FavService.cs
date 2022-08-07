a245192a304a3ae6de1ed8d3fc0b9ff0284ddf86
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-08-07 20:26:55 +0300

    UI service completed (Fav)

---
+++ Clients/BlazorWeb/CarForum.WebApp/Infrastructure/Services/FavService.cs
@@ -0,0 +1,28 @@
+﻿namespace CarForum.WebApp.Infrastructure.Services
+{
+    public class FavService : IFavService
+    {
+        private readonly HttpClient _client;
+
+        public FavService(HttpClient httpClient)
+        {
+            _client = httpClient;
+        }
+        public async Task CreateEntryFav(Guid entryId)
+        {
+            await _client.PostAsync($"/api/favorite/Entry/{entryId}", null);
+        }
+        public async Task CreateEntryCommentFav(Guid entryCommentId)
+        {
+            await _client.PostAsync($"/api/favorie/EntryComment/{entryCommentId}", null);
+        }
+        public async Task DeleteEntryFav(Guid entryId)
+        {
+            await _client.PostAsync($"/api/favorite/DeleteEntryFav/{entryId}", null);
+        }
+        public async Task DeleteEntryCommentFav(Guid entryCommentId)
+        {
+            await _client.PostAsync($"/api/favorite/DeleteEntryCommentFav/{entryCommentId}", null);
+        }
+    }
+}
